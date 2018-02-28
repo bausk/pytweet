2be05186af981ebb88cd246989ee124a2503fdc7
Alex Bausk <bauskas@gmail.com>
2018-02-28 22:19:09 +0200

    Pandas analytics working

--- persistence/pandas.py
+++ persistence/pandas.py
@@ -25,13 +25,49 @@ class PandasReader(TimeSeriesStore):
         cur = connection.cursor()
 
         now = datetime.utcnow()
+        start = now - timedelta(hours=start)
+        end = now - timedelta(hours=end)
+
         if self._last_queried is None or now - self._last_queried > timedelta(minutes=5):
             self._refresh_segments(now, cur)
 
-
-        # TODO: figure out which trunks to load
+        self._segments_df.sort_index(inplace=True)
+        segments_to_query = list(self._segments_df.loc[
+            (start <= self._segments_df.index)
+            & (self._segments_df.index <= end)
+            & (self._segments_df.loaded == False)
+                                 ]['id'].tolist())
+        print(segments_to_query)
+        # TODO: figure out which additional trunks to load
+        # TODO: 1) One previous trunk
+        try:
+            prev_segment = self._segments_df.loc[(self._segments_df.index < start)].iloc[-1]
+            if not prev_segment['loaded']:
+                prev_segment_id = int(prev_segment['id'])
+            else:
+                prev_segment_id = None
+        except:
+            prev_segment_id = None
+        if prev_segment_id is not None:
+            segments_to_query.append(prev_segment_id)
+        # TODO: 2) One latest trunk
         if end == 0:
-            pass
+            last_segment_id = self._get_last_trunk_id(cur)
+            if last_segment_id not in segments_to_query:
+                segments_to_query.append(last_segment_id)
+        if len(segments_to_query) > 0:
+            command = """
+            select * from public.{name}
+            WHERE id IN %s;
+            """.format(name=self.name)
+            cur.execute(command, (tuple(segments_to_query), ))
+            res = cur.fetchall()
+            res = [self._row(x) for x in res]
+            self._segments_df.loc[self._segments_df.id.isin(segments_to_query), 'loaded'] = True
+            return self._update_internal_df(self._remote_to_df(res))
+        else:
+            return self._df
+
 
     def _refresh_segments(self, now, cur):
         command = """
@@ -41,12 +77,21 @@ class PandasReader(TimeSeriesStore):
         """.format(name=self.name)
         cur.execute(command)
         res = cur.fetchall()
-
+        data = {x[1]: dict(id=x[0], created_at=x[1], loaded=False) for x in res}
+        _df = pd.DataFrame.from_dict(data, orient='index')
+        _df['Time'] = _df.index
+        _df.set_index('Time', inplace=True)
+        self._segments_df = _df
+        self._last_queried = now
+        return _df
 
     def _load_latest_trunks(self, trunks):
         result = self._perform_limited_pure_load(trunks=trunks)
         self._loaded_chunks.update({trunk['id']: dict(id=trunk['id'], created_at=trunk['created_at']) for trunk in result})
         result = self._remote_to_df(result)
+        return self._update_internal_df(result)
+
+    def _update_internal_df(self, result):
         if self._df is not None:
             self._df = pd.concat([self._df, result]).drop_duplicates()
             self._df.sort_index(inplace=True)
